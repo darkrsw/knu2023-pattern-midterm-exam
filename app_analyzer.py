c0a2a94cae72df27e686228359c9d115241cd529
Dongsun Kim <darkrsw@gmail.com>
2023-10-22 12:11:53 +0900

    init.

---
+++ app_analyzer.py
@@ -0,0 +1,10 @@
+from javalang.parse import parse
+import javalang
+from typing import Any, Dict, List, Set, Optional, Union, Tuple, Type
+
+from os import listdir
+from os.path import isfile, join
+
+
+def collectAccessMapFromPath(path: str):
+    return class_map
